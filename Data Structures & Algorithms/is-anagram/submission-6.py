a0e6edaecf9e26b48930c49905ff27e5277ab9ff
S HAYAGREEV <hayagreev2003@gmail.com>
2026-05-24 10:50:29 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hd1={}
+        for i in s:
+            hd1[i]=hd1.get(i,0)+1
+        for j in t:
+            if j not in hd1:
+                return False
+            else:
+                hd1[j]=hd1.get(j,0)-1
+                if hd1[j]<0:
+                    return False
+        for d in hd1:
+            if hd1[d]!=0:
+                return False
+        return True
